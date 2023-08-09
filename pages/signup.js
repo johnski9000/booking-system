68e314f35c70200e692d5f9818c0fb5f800a5ea5
Janusz Wozniak <januszwozniak.bth@gmail.com>
2023-08-09 16:49:26 +0100

    react import

--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,6 @@
 "use client"
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import styles from "../styles/Home.module.css";
 import { useRouter } from 'next/navigation'
 import signUp from "./firebase/signup";
